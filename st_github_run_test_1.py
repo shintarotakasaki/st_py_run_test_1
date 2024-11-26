1e64ca770118d2b921782ed953aab26320050b9d
shintarotakasaki <137417713+shintarotakasaki@users.noreply.github.com>
2024-11-26 12:08:52 +0900

    Update st_github_run_test_1.py

--- st_github_run_test_1.py
+++ st_github_run_test_1.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1N6-V8VDLuOWmmiskMwEZHNXE4nSHyptL
 """
 
-import streamlit as st
 import subprocess
 import os
 import sys
@@ -20,9 +19,9 @@ sys.path.append('st_py_run_test_1')
 import st_run
 
 def main():
+    import streamlit as st
     st.title("Streamlit App")
-    result1 = st_run.some_function()
-    st.write(resurut1)
-
+    import st_run
+    
 if __name__ == "__main__":
     main()
