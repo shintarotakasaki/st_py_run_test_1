1fda911c69687f9c04741952c69da091b2535a74
shintarotakasaki <137417713+shintarotakasaki@users.noreply.github.com>
2024-11-26 11:14:24 +0900

    Update st_github_run_test_1.py

--- st_github_run_test_1.py
+++ st_github_run_test_1.py
@@ -10,7 +10,19 @@ Original file is located at
 import streamlit as st
 import subprocess
 import os
+import sys
 
-# Streamlitアプリを実行
-os.system('streamlit run st_py_run_test_1/st_run.py')
+if not os.path.exests('st_py_run_test_1'):
+    subprocess.run(['git','clone','https://github.com/shintarotakasaki/st_py_run_test_1.git'])
 
+sys.path.append('st_py_run_test_1')
+
+import st_run
+
+def main():
+    st.title("Streamlit App")
+    result1 = st_run.some_function()
+    st.write(resurut1)
+
+if __name__ == "__main__":
+    main()
