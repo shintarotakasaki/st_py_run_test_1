a41da21eebf831117a729970f0ff1dc1ccb9513b
shintarotakasaki <137417713+shintarotakasaki@users.noreply.github.com>
2025-01-06 16:24:39 +0900

    Update gitpy_run_test.py

--- gitpy_run_test.py
+++ gitpy_run_test.py
@@ -22,22 +22,24 @@ sys.path.append('st_py_run_test_1')
 import pdf_des
 import xl_des
 
-st.title("Streamlitでレポジトリ内のpyを実行")
-after_xl = st.file_uploader("ファイルををアップロードしてください")
+def main():
+    import streamlit as st
+    st.title("Streamlitでレポジトリ内のpyを実行")
+    after_xl = st.file_uploader("ファイルををアップロードしてください")
 
-if after_xl is not None:
-    file_mime = after_xl.type
+    if after_xl is not None:
+        file_mime = after_xl.type
 
-    if file_mime == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-        file = BytesIO(after_xl.getvalue())
-        wb = load_workbook(filename=file)
-        sheet = wb.active
-        import xl.des
-        st.write(f"Sheet title: {sheet.title}")
+        if file_mime == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+            file = BytesIO(after_xl.getvalue())
+            wb = load_workbook(filename=file)
+            sheet = wb.active
+            import xl.des
+            st.write(f"Sheet title: {sheet.title}")
 
-    elif file_mime=='application/pdf':
-        import pdf.des
+        elif file_mime=='application/pdf':
+            import pdf.des
 
-    else:
-        st.write("エクセルファイル(.xlsx)をアップロードしてください")
-        st.stop()
+        else:
+            st.write("エクセルファイル(.xlsx)をアップロードしてください")
+            st.stop()
